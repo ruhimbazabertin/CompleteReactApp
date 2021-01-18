5a643c542224fd29c30c98fe11d373bc72329624
bertin <ruhimbazab@gmail.com>
2021-01-17 22:02:48 -0800

    the use of hooks
    adding hooks folder
    adding useVideos component
    
    hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import SearBar from './SearchBar';
-import Youtube from './apis/youtube';
 import VideoDetail from './VideoDetail';
 import VideoList from './VideoList';
+import useVodeos from '../hooks/useVideos';
 
 const App = () => {
 
-const [videos, setVideos] = useState([]);
 const [selectedVideo, setSelectedVideo] = useState(null);
 
-useEffect(() => {
+ const [videos, search] =  useVodeos('kids stories');
 
-    onVideoSearch('kids stories');
-
-}, []);
-
-const onVideoSearch = async (term) => {
-    const response = await Youtube.get('/search',{
-        params: {
-            q: term,
-        }
-    });
-      setVideos(response.data.items);
-        setSelectedVideo(response.data.items[0]);
-      };
-
+ useEffect(() => {
+    setSelectedVideo(videos[0]);
+ }, [videos]);
 
 return(
     <div className="ui container">
-        <SearBar onSearch={onVideoSearch}/>
+        <SearBar onSearch={search}/>
         {/* Wiring VideoDetail component in App */}
         <div className="ui grid">
         <div className="ui row">
